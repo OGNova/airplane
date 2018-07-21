c007162ad2b5459ee1ee111edc4cf20d76c5d6b0
Terminator966 <msimeone96@gmail.com>
2018-07-21 09:02:22 -0400

    Reverted attempted fix on infractions failing to expire.

--- rowboat/plugins/infractions.py
+++ rowboat/plugins/infractions.py
@@ -76,7 +76,7 @@ class InfractionsPlugin(Plugin):
     def clear_infractions(self):
         expired = list(Infraction.select().where(
             (Infraction.active == 1) &
-            (Infraction.expires_at <= datetime.utcnow()) #testing to fix temp inf counting up
+            (Infraction.expires_at < datetime.utcnow()) #testing to fix temp inf counting up
         ))
 
         self.log.info('[INF] attempting to clear %s expired infractions', len(expired))
