a51756e5f7204ccaa442c6bf765aa189e3e91f69
OGNova <jamooney2000@gmail.com>
2018-07-23 23:46:49 -0400

    Update bot info.

--- rowboat/plugins/core.py
+++ rowboat/plugins/core.py
@@ -48,7 +48,7 @@ from yaml import load
 PY_CODE_BLOCK = u'```py\n{}\n```'
 
 BOT_INFO = '''
-Airplane is a moderation and utilitarian bot.
+Airplane is a utilitarian and administration assistant for Discord. It's a modified version of the bot Rowboat that was built to be highly configurable, and allow server admins to modify functionality based on the requirements of individual servers. The freely hosted version of airplane is private, and only added on high-traffic servers. Airplane is open-source.
 '''
 
 GUILDS_WAITING_SETUP_KEY = 'gws'
