4e51244f60635ab51ebf2dbb2a795a39a8c2a8c2
Terminator966 <msimeone96@gmail.com>
2018-07-27 21:33:31 -0400

    if this doesn't work idk what will lul

--- rowboat/plugins/memes.py
+++ rowboat/plugins/memes.py
@@ -22,33 +22,23 @@ class MemesPlugin(Plugin):
     def meesucks_listener(self, event):
         if event.config.hate_meesux is False:
             return
-        if event.message.author.id != 159985870458322944:
+        if event.msg.author.id != 159985870458322944:
             return
-        event.message.reply('<@159985870458322944> **NO ONE CARES.**')
+        event.msg.reply('<@159985870458322944> **NO ONE CARES.**')
         
 
     @Plugin.listen('MessageCreate')
     def alexa_play_despacito_listener(self, event):
-        if "alexa play despacito" in event.message.content and event.author.id == 191793155685744640:
+        if "alexa play despacito" in event.msg.content and event.author.id == 191793155685744640:
             def g():
-                event.message.reply('des')
+                event.msg.reply('des')
                 gevent.sleep(.5)
-                event.message.reply('pa')
+                event.msg.reply('pa')
                 gevent.sleep(.5)
-                event.message.reply('cito')
+                event.msg.reply('cito')
             gevent.spawn(g) 
 
     @Plugin.command('pong', level=-1)
     def pong(self, event):
-        event.message.reply('I pong, you ping. Idiot...')
+        event.msg.reply('I pong, you ping. Idiot...')
 
-    # @disco.bot.plugin.BasePluginDeco.listen("MessageCreate")
-    # def on_message_create(self, event):
-    #     if "alexa play despacito" in event.message.content and event.author.id == 191793155685744640:
-    #         def f():
-    #             event.message.reply('des')
-    #             gevent.sleep(.5)
-    #             event.message.reply('pa')
-    #             gevent.sleep(.5)
-    #             event.message.reply('cito')
-    #         gevent.spawn(f)
