96a23126f5b3ad14663490b00c9798806855bd1d
Terminator966 <msimeone96@gmail.com>
2018-07-29 16:58:54 -0400

    Overload. Added amazon alexa

--- rowboat/plugins/memes.py
+++ rowboat/plugins/memes.py
@@ -21,14 +21,31 @@ class MemesPlugin(Plugin):
     
     @Plugin.listen('MessageCreate')
     def alexa_play_despacito_listener(self, event):
-        if "alexa play despacito" in event.content.lower() and event.author.id == 191793155685744640:
-            def g():
-                event.channel.send_message('des')
-                gevent.sleep(.5)
-                event.channel.send_message('pa')
-                gevent.sleep(.5)
-                event.channel.send_message('cito')
-            gevent.spawn(g) 
+        if "hey alexa" in event.content.lower() and event.author.id == 191793155685744640:
+            msg = event.channel.send_message('<a:hey_alexa:473213508171989023>')
+            try:
+                alexa_event = self.wait_for_event(
+                    'MessageCreate',
+                    conditional=lambda e: (
+                        e.author.id == event.author.id
+                    )).get(timeout=10)
+                #  despacito
+                if "play despacito" in alexa_event.content.lower():
+                    msg.delete()
+                    event.channel.send_message('Ok, playing Despacito on <:spotify:473223054831517727>')
+                    def g():
+                        event.channel.send_message('des')
+                        gevent.sleep(.5)
+                        event.channel.send_message('pa')
+                        gevent.sleep(.5)
+                        event.channel.send_message('cito')
+                    gevent.spawn(g)
+            except gevent.Timeout:
+                msg.delete()
+                return
+            
+
+
     @Plugin.listen('MessageCreate')
     def meesucks_listener(self, event):
         if event.config.hate_meesux is True:
