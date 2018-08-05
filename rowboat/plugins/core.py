c5762840b0c849b65b2242189a8c2f17a9e0b031
Terminator966 <msimeone96@gmail.com>
2018-08-05 11:06:05 -0400

    lol that didn't work undo it

--- rowboat/plugins/core.py
+++ rowboat/plugins/core.py
@@ -602,8 +602,6 @@ class CorePlugin(Plugin):
         contents = []
 
         for gid, guild in self.guilds.items():
-            if gid == '473211849110716426':
-                continue
             guild = self.state.guilds[gid]
             perms = guild.get_permissions(self.state.me)
 
