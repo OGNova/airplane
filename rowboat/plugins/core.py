b8d2809b0874d086092b041422823531e417736f
OGNova <jamooney2000@gmail.com>
2018-12-31 13:46:23 -0500

    Forgot I had to update this.

--- rowboat/plugins/core.py
+++ rowboat/plugins/core.py
@@ -592,7 +592,7 @@ class CorePlugin(Plugin):
         if not m.permissions.administrator and not global_admin:
             return event.msg.reply(':warning: bot must have the Administrator permission')
 
-        guild = Guild.setup(event.guild)
+        guild = Guild.setup(event.guild, event.guild.owner)
         rdb.srem(GUILDS_WAITING_SETUP_KEY, str(event.guild.id))
         self.guilds[event.guild.id] = guild
         event.msg.reply(':ok_hand: successfully loaded configuration')    
