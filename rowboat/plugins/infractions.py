2a77d45d035192a5ffb79b11f53afc99db41e8a3
OGNova <jamooney2000@gmail.com>
2019-01-08 04:58:46 -0500

    This is better.

--- rowboat/plugins/infractions.py
+++ rowboat/plugins/infractions.py
@@ -332,7 +332,7 @@ class InfractionsPlugin(Plugin):
         ).where(q).order_by(Infraction.created_at.desc()).limit(8)
 
         if not infractions:
-            return event.msg.reply('No infractions found for the given query.')
+            return event.msg.reply('No infractions found.')
 
         tbl = MessageTable()
 
