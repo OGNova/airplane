9b8a2836202b82ae91e6ed78133dfb10a02b4bf8
Terminator966 <msimeone96@gmail.com>
2019-03-01 14:36:21 -0500

    error fix and cleenup

--- rowboat/plugins/memes.py
+++ rowboat/plugins/memes.py
@@ -280,23 +280,21 @@ class MemesPlugin(Plugin):
                         )).get(timeout=15)
                 except gevent.Timeout:
                     prompt_p1.delete()
-                    event.msg.reply('Game canceled, {} failed to make their choice.'.format(p_1[0].mention))
+                    event.msg.reply('Game canceled, {} failed to make their choice.'.format(p_1[0].mention)).after(5).delete()
+                    return
                 if mra_event.emoji.id == game_emotes_rps['rock']['default']['id']:
                     p_1.append('rock')
                 elif mra_event.emoji.id == game_emotes_rps['paper']['default']['id']:
                     p_1.append('paper')
                 elif mra_event.emoji.id == game_emotes_rps['scissors']['default']['id']:
                     p_1.append('scissors')
-                else:
-                    temp.delete()
-                    raise CommandFail('invalid emoji selected.')
                 temp.delete()
+                prompt_p1.delete()
                 try: # Send dm to second user
                     prompt_p2 = p_2[0].user.open_dm().send_message('{}, Rock, Paper, Scissors says shoot! (Please react to one of the following).'.format(p_2[0].mention))
                 except:
                     event.msg.reply('{0}, your DMs are disabled, therefore you are unable play other users. Please open your DMs and try again.'.format(p_2[0].mention)).after(5).delete()
                     return
-                prompt_p1.delete()
                 temp = event.msg.reply('{} is selecting their choice, please wait.'.format(p_2[0].user))
                 prompt_p2.chain(False).\
                     add_reaction(game_emotes_rps['rock']['default']['emote']).\
@@ -312,16 +310,14 @@ class MemesPlugin(Plugin):
                         )).get(timeout=15)
                 except gevent.Timeout:
                     prompt_p2.delete()
-                    event.msg.reply('Game canceled, {} failed to make their choice.'.format(p_2[0].mention))
+                    event.msg.reply('Game canceled, {} failed to make their choice.'.format(p_2[0].mention)).after(5).delete()
+                    return
                 if mra_event.emoji.id == game_emotes_rps['rock']['default']['id']:
                     p_2.append('rock')
                 elif mra_event.emoji.id == game_emotes_rps['paper']['default']['id']:
                     p_2.append('paper')
                 elif mra_event.emoji.id == game_emotes_rps['scissors']['default']['id']:
                     p_2.append('scissors')
-                else:
-                    temp.delete()
-                    raise CommandFail('invalid emoji selected.')
                 prompt_p2.delete()
                 temp.delete()
                 outcome = winner_rps(p_1[1], p_2[1])
